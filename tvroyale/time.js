83d020e670631dbd7b30796d99e325924eda6be3
megafrog2000 <megafrog2000@protonmail.com>
2025-04-03 23:06:51 -0400

    Add files via upload

---
+++ tvroyale/time.js
@@ -0,0 +1,13 @@
+﻿function gettime() {
+    time = document.getElementById("time");
+    t = new Date();
+    min = '';
+    if (t.getMinutes() < 10) {
+       min = '0' + t.getMinutes()
+    }
+    else {
+        min = t.getMinutes()
+    }
+    fulltime = t.getHours() + ':' + min;
+    time.innerText = fulltime
+}
